9a0b4a0e9ee041c46015865663584472242cc705
Sarah Jameson <meowells@gmail.com>
2022-07-05 16:16:24 +0000

    Setup shopping bag app

---
+++ shopping_bag/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def view_shopping_bag(request):
+    """A view to return the shopping bag page"""
+    return render(request, 'shopping_bag/shopping_bag.html')
