177afbf6ca55c9f66f183682c56a684742bd7c65
Sarah Jameson <meowells@gmail.com>
2022-07-09 23:04:56 +0000

    Got stripe webhooks to work

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_successful<order_number>', views.checkout_successful, name='checkout_successful'),
+    path('cache_checkout_data', views.cache_checkout_data, name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),
 ]
