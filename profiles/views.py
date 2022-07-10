4db3018f8beef0b2b06d6f56a1f80ca19472fd8b
Sarah Jameson <meowells@gmail.com>
2022-07-10 10:28:28 +0000

    Worked on profile app. Add views and template

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from checkout.models import Order
+from .models import UsersProfile
+from .forms import UsersProfileForm
 
+
+@login_required
 def profile(request):
-    """ Display the user's profile. """
+    """
+    Renders the profile page
+    """
+    profile = get_object_or_404(UsersProfile, user=request.user)
+    orders = profile.orders.all()
+    form = UsersProfileForm(instance=profile)
+    if not profile:
+        messages.error(request,
+                       "I'm sorry, you need to log in first")
+        return redirect(reverse('home'))
+    if request.method == 'POST':
+        form = UsersProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
 
     return render(request, template, context)
