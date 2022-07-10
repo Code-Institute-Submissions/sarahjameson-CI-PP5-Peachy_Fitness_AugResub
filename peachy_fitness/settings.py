007da4228c5b0bb17e74810f0010ae81287605ee
Sarah Jameson <meowells@gmail.com>
2022-07-10 17:14:06 +0000

    Added heroku app to allowed hosts and disabled collect static

--- peachy_fitness/settings.py
+++ peachy_fitness/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-y!exmm+p^)m&^x^jef1!3g2rvsp@fqt$wz$%=3mbu^uyxr(*fj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['peachy_fitness.herokuapp.com', localhost]
 
 
 # Application definition
@@ -119,17 +119,20 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
